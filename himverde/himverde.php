eaeecdde1dc8da0c44a42f83b63455fe645321f6
ismaelbob <bobismaeljg@gmail.com>
2021-04-22 17:34:51 -0400

    Agregue el poder editar himnario verde

--- himverde/himverde.php
+++ himverde/himverde.php
@@ -8,10 +8,16 @@
 
         public function datosCancion($id) {
             $con = $this -> conectar() -> prepare("SELECT * FROM him_verde WHERE idcancion=:id");
-            $con -> execute(["id" => $id]);
+            $con -> execute([':id' => $id]);
             $datos = $con -> fetch(PDO::FETCH_OBJ);
             $con = null;
             return $datos;
+        }
+        public function editarCancion($id, $titulo, $autor, $nota, $letra, $enlace) {
+            $con = $this -> conectar() -> prepare('UPDATE him_verde SET titulo=:titulo, autor=:autor, nota=:nota, letra=:letra, enlace=:enlace WHERE idcancion=:id;');
+            $con -> execute([':titulo' => $titulo, ':autor' => $autor, ':nota' => $nota, ':letra' => $letra, ':enlace' => $enlace, ':id' => $id]);
+            $con = null;
+            return 'correcto';
         }
     }
 ?>
