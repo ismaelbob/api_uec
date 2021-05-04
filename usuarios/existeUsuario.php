85be44da48131ccbe4349d56377fba72deaf8b12
ismaelbob <bobismaeljg@gmail.com>
2021-05-04 17:00:59 -0400

    Se aunmento servicio de usuarios

---
+++ usuarios/existeUsuario.php
@@ -0,0 +1,36 @@
+<?php
+    include_once 'usuario.php';
+    $usuario = new Usuario();
+    header('Access-control-Allow-Origin: *');
+
+    $data = json_decode(file_get_contents('php://input'));
+
+    if ($data === null) {
+        $respuesta = [
+            'estado' => 'No hay datos'
+        ];
+    } else {
+        $user = $data -> usuario;
+        $pass = $data -> pass;
+
+        $resultado = $usuario -> existeUsuario($user, $pass);
+
+        if ($resultado) {
+            $respuesta = [
+                'estado' => 'correcto',
+                'usuario' => $user,
+                'pass' => $pass,
+            ];
+        } else {
+            $respuesta = [
+                'estado' => 'incorrecto',
+                'usuario' => $usuario,
+                'pass' => $pass,
+            ];
+        }
+
+    }
+
+    $respuestaCodificada = json_encode($respuesta);
+    echo $respuestaCodificada;
+?>
