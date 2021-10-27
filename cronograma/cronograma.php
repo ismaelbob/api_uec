6127c6d0166e0f729c3bb119d8f7b21a11aee535
bob <bobismaeljg@gmail.com>
2021-10-27 15:27:07 -0400

    Se agrego get de los turnos

--- cronograma/cronograma.php
+++ cronograma/cronograma.php
@@ -5,5 +5,11 @@
         public function getMinisterios () {
             return $con = $this -> conectar() -> query ('SELECT * FROM ministerio');
         }
+        public function getTurnoSemana ($mes, $semana) {
+            return $con = $this -> conectar() -> query ('SELECT * FROM mes JOIN semana ON mes.idmes=semana.idmes JOIN ministerio ON ministerio.idministerio=semana.idgrupo WHERE mes.idmes=$mes AND semana.idsemana=$semana')
+        }
+        public function getTurnoMensual () {
+            return $con = $this -> conectar() -> query ('SELECT * FROM mes JOIN semana ON mes.idmes=semana.idmes JOIN ministerio ON ministerio.idministerio=semana.idgrupo')
+        }
     }
 ?>
