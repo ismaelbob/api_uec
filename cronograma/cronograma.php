912c16ead6491a7064903be154204141abcda942
bob <bobismaeljg@gmail.com>
2021-11-02 13:35:32 -0400

    Se preparo sql para que traiga solo un mes

--- cronograma/cronograma.php
+++ cronograma/cronograma.php
@@ -13,10 +13,10 @@
             return $datos;
         }
         public function getTurnoMensual () {
-            return $con = $this -> conectar() -> query ('SELECT * FROM mes JOIN semana ON mes.idmes=semana.idmes JOIN ministerio ON ministerio.idministerio=semana.idgrupo');
+            return $con = $this -> conectar() -> query ('SELECT * FROM mes JOIN semana ON mes.idmes=semana.idmes JOIN ministerio ON ministerio.idministerio=semana.idgrupo WHERE  mes.idmes=2');
         }
         public function getTurnoMensualJovenes () {
-            return $con = $this -> conectar() -> query ('SELECT * FROM mes JOIN semana_jov ON mes.idmes=semana_jov.idmes JOIN ministerio ON ministerio.idministerio=semana_jov.idgrupo');
+            return $con = $this -> conectar() -> query ('SELECT * FROM mes JOIN semana_jov ON mes.idmes=semana_jov.idmes JOIN ministerio ON ministerio.idministerio=semana_jov.idgrupo WHERE mes.idmes=2');
         }
     }
 ?>
