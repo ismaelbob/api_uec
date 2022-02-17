a0e5d7da7be250823668714ddd3d26199b15a531
bob <bobismaeljg@gmail.com>
2022-02-17 12:29:42 -0400

    Solucione falla en la consulta SQL de setTurnoJovenes

--- cronograma/cronograma.php
+++ cronograma/cronograma.php
@@ -19,8 +19,8 @@
             return 'correcto';
         }
         public function setTurnoJovenes ($idsemana_jov, $idmes, $idgrupo, $fecha) {
-            $con = $this -> conectar() -> prepare ('UPDATE semana_jov SET idmes=:mes, idgrupo=:grupo, fecha=:fec WHERE idsemana_jov=$idsemana_jov');
-            $con -> execute([':mes' => $idmes, ':grupo' => $idgrupo, ':fec' => $fecha, ':idsemana_jov' => $idsemana_jov]);
+            $con = $this -> conectar() -> prepare ('UPDATE semana_jov SET idmes=:mes, idgrupo=:grupo, fecha=:fec WHERE idsemana_jov=:id;');
+            $con -> execute([':mes' => $idmes, ':grupo' => $idgrupo, ':fec' => $fecha, ':id' => $idsemana_jov]);
             $con = null;
             return 'correcto';
             
